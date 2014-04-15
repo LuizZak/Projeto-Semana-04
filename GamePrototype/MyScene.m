d1b3275e8a58810de22d7df9f4cf490f9e2dd2a2
Tutz <ttutty.oliveira@gmail.com>
2014-04-15 18:13:05 -0300

    melhorando o código

--- GamePrototype/MyScene.m
+++ GamePrototype/MyScene.m
@@ -51,13 +51,6 @@
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    SKTransition *reveal = [SKTransition fadeWithDuration:1.0];
-    //WorldMap *battleScene = [[WorldMap alloc] initWithSize:self.size];
-    [self.scene.view presentScene:[[GameData gameData] world] transition: reveal];
-}
-
 - (void)createEnemy:(float)x y:(float)y
 {
     SKNode *enemyNode = [SKSpriteNode spriteNodeWithColor:[UIColor purpleColor] size:CGSizeMake(30, 30)];
@@ -70,6 +63,13 @@
     enemyNode.position = CGPointMake(x, y);
     
     [self addEntity:enemy];
+}
+
+- (void)fecharATela
+{
+    SKTransition *reveal = [SKTransition fadeWithDuration:1.0];
+    //WorldMap *battleScene = [[WorldMap alloc] initWithSize:self.size];
+    [self.scene.view presentScene:[[GameData gameData] world] transition: reveal];
 }
 
 @end

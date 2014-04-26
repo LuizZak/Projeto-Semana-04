b7f6dbaedeae4162551437c3c791c7bc3f4b337d
Luiz Fernando <luizinho_mack@yahoo.com.br>
2014-04-26 17:18:47 -0300

    Criando classe de configuração de batalha

---
+++ GamePrototype/BattleConfig.h
@@ -0,0 +1,17 @@
+//
+//  BattleConfig.h
+//  GamePrototype
+//
+//  Created by Luiz Fernando Silva on 26/04/14.
+//  Copyright (c) 2014 LUIZ FERNANDO SILVA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Classe que mantém configurações de batalha
+@interface BattleConfig : NSObject
+
+// Array de entidades de inimigos
+@property NSMutableArray *enemiesArray;
+
+@end
---
+++ GamePrototype/BattleConfig.m
@@ -0,0 +1,13 @@
+//
+//  BattleConfig.m
+//  GamePrototype
+//
+//  Created by Luiz Fernando Silva on 26/04/14.
+//  Copyright (c) 2014 LUIZ FERNANDO SILVA. All rights reserved.
+//
+
+#import "BattleConfig.h"
+
+@implementation BattleConfig
+
+@end

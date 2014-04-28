f73ae02f801909f76efa7d7afdcc0ac34c3ee640
Luiz Fernando <luizinho_mack@yahoo.com.br>
2014-04-28 14:12:54 -0300

    Arrumando alguns defines

--- GamePrototype/CommonImports.h
+++ GamePrototype/CommonImports.h
@@ -10,23 +10,28 @@
 #define GamePrototype_CommonImports_h
 
 #define PLAYER_ID 1
-#define MAP_ID 2
+#define MAP_ID    2
 #define CAMERA_ID 3
 
 #define PLAYER_TYPE 0x1 << 0
-#define ENEMY_TYPE 0x1 << 1
+#define ENEMY_TYPE  0x1 << 1
 
-#define DIR_TOP 1
-#define DIR_RIGHT 2
-#define DIR_LEFT 4
+#define DIR_TOP    1
+#define DIR_RIGHT  2
+#define DIR_LEFT   4
 #define DIR_BOTTOM 8
 
-#define TILE_GRASS 0
-#define TILE_MONTAIN 1
-#define TILE_EARTH 2
-#define TILE_CAVE 3
+#define TILE_GRASS        0
+#define TILE_MONTAIN      1
+#define TILE_EARTH        2
+#define TILE_CAVE         3
 #define TILE_CASTLE_EARTH 4
 #define TILE_CASTLE_GRASS 5
-#define TILE_WATER 6
+#define TILE_WATER        6
+
+// Defines de Batalha
+#define FIREBALL_BITMASK 0x1 << 0
+#define ENEMY_BITMASK    0x1 << 1
+#define PLAYER_BITMASK   0x1 << 2
 
 #endif

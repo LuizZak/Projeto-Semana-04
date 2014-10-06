5d65ab12f181dbe9ac6af41076d24c0fbe1a24ac
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2014-10-05 23:21:05 -0300

    Adicionando classe ActionPieView para exibição de menu de escolha de ações

---
+++ GamePrototype/ActionPieView.h
@@ -0,0 +1,70 @@
+//
+//  ActionPieView.h
+//  GamePrototype
+//
+//  Created by Luiz Fernando Silva on 05/10/14.
+//  Copyright (c) 2014 LUIZ FERNANDO SILVA. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+#import "ActionCollection.h"
+#import "GPGameScene.h"
+
+/// Describes the orientation of an ActionPieView
+typedef enum ActionPieViewMenuOrientationEnum {
+    /// Automatically chooses the orientation to display the ActionPieMenu
+    ActionPieViewMenuOrientationAuto,
+    /// Specifies that a menu is pointing up
+    ActionPieViewMenuOrientationTop,
+    /// Specifies that a menu is pointing left
+    ActionPieViewMenuOrientationLeft,
+    /// Specifies that a menu is pointing bottom
+    ActionPieViewMenuOrientationBottom,
+    /// Specifies that a menu is pointing right
+    ActionPieViewMenuOrientationRight
+} ActionPieViewMenuOrientation;
+
+/// Pie view used to display a collection of actions on top of a point
+@interface ActionPieView : SKNode <GPGameSceneNotifier>
+{
+    /// Whether this ActionPieView is currently in the process of closing itself
+    BOOL closing;
+}
+
+/// The radius of the pie menu
+@property CGFloat pieRadius;
+
+/// The radius to layout the items of the pie menu on, in degrees
+@property CGFloat arcRadius;
+
+/// Whether to allow this ActionPieView to organize the actions into categories that are displayed in sub-menus
+@property BOOL organizeCategories;
+
+/// The action collection associated with this ActionPieView
+@property ActionCollection *actionCollection;
+
+/// A reference to the parent menu of this ActionPieMenu, when available
+@property (weak) ActionPieView *parentMenu;
+
+/// A reference to the child menu of this ActionPieMenu, when available
+@property ActionPieView *childMenu;
+
+/// The orientation to display this ActionPieMenu
+@property ActionPieViewMenuOrientation orientation;
+
+/// Initializes this ActionPieView with a given ActionCollection
+- (id)initWithActionCollection:(ActionCollection*)collection;
+
+/// Opens this menu with a given orientation at a given point
+- (void)open:(ActionPieViewMenuOrientation)orientation onNode:(SKNode*)node atPoint:(CGPoint)point;
+
+/// Closes this menu and all children menus opened on this ActionPieView
+- (void)close;
+
+/// Called whenever the first immediate child of this ActionPieView has closed itself
+- (void)childClosed;
+
+/// Opens up a submenu on this ActionPieMenu on a given item index
+- (void)openSubMenu:(ActionCollection*)collection onIndex:(NSInteger)index;
+
+@end
---
+++ GamePrototype/ActionPieView.m
@@ -0,0 +1,234 @@
+//
+//  ActionPieView.m
+//  GamePrototype
+//
+//  Created by Luiz Fernando Silva on 05/10/14.
+//  Copyright (c) 2014 LUIZ FERNANDO SILVA. All rights reserved.
+//
+
+#import "ActionPieView.h"
+#import "ActionIconView.h"
+
+@implementation ActionPieView
+
+- (id)initWithActionCollection:(ActionCollection *)collection
+{
+    self = [super init];
+    if (self)
+    {
+        closing = NO;
+        
+        self.arcRadius = 135;
+        self.pieRadius = 100;
+        self.organizeCategories = YES;
+        self.actionCollection = collection;
+    }
+    return self;
+}
+
+- (void)open:(ActionPieViewMenuOrientation)orientation onNode:(SKNode *)node atPoint:(CGPoint)point
+{
+    // Auto orientation scheme
+    if(orientation == ActionPieViewMenuOrientationAuto)
+    {
+        CGSize screenSize = node.scene.size;
+        CGPoint screenCenter = CGPointMake(screenSize.width / 2, screenSize.height / 2);
+        
+        CGFloat angle = atan2(point.y - screenCenter.y, -point.x - screenCenter.x) * -(180 / M_PI);
+        
+        angle = floor(angle / 90) * 90;
+        
+        if(point.x > screenSize.width / 2)
+        {
+            orientation = ActionPieViewMenuOrientationLeft;
+        }
+        else
+        // Right side
+        if(angle == 0)
+        {
+            orientation = ActionPieViewMenuOrientationLeft;
+        }
+        // Top
+        else if(angle == 90)
+        {
+            orientation = ActionPieViewMenuOrientationBottom;
+        }
+        // Left side
+        else if(angle == 180 || angle == -180)
+        {
+            orientation = ActionPieViewMenuOrientationRight;
+        }
+        // Bottom side
+        else if(angle == -90)
+        {
+            orientation = ActionPieViewMenuOrientationTop;
+        }
+    }
+    
+    self.orientation = orientation;
+    
+    // Add pie menu to node
+    self.position = point;
+    [node addChild:self];
+    
+    [self createIcons];
+}
+
+- (void)close
+{
+    closing = YES;
+    
+    // Close children menu first
+    if(self.childMenu)
+    {
+        [self.childMenu close];
+    }
+    
+    [self removeFromParent];
+    
+    if(self.parentMenu)
+    {
+        [self.parentMenu childClosed];
+        self.parentMenu = nil;
+    }
+}
+
+- (void)childClosed
+{
+    self.childMenu = nil;
+}
+
+- (void)openSubMenu:(ActionCollection *)collection onIndex:(NSInteger)index
+{
+    
+}
+
+// Private members
+- (void)createIcons
+{
+    // List of icons to display
+    NSMutableArray *iconsArray = [self getActionIconViewList];
+    
+    // Calculate the angles now
+    CGFloat startAngle = 0;
+    
+    switch (self.orientation) {
+        case ActionPieViewMenuOrientationBottom:
+            startAngle = -90;
+            break;
+        case ActionPieViewMenuOrientationLeft:
+            startAngle = 180;
+            break;
+        case ActionPieViewMenuOrientationRight:
+            startAngle = 0;
+            break;
+        case ActionPieViewMenuOrientationTop:
+            startAngle = 90;
+        default:
+            startAngle = 0;
+            break;
+    }
+    
+    startAngle -= self.arcRadius / 2;
+    CGFloat stepAngle = self.arcRadius / iconsArray.count;
+    
+    for (int i = 0; i < iconsArray.count; i++)
+    {
+        CGFloat iconAngle = startAngle + stepAngle * i + stepAngle / 2;
+        
+        CGPoint point = CGPointMake(cos(iconAngle * (M_PI / 180)) * self.pieRadius, sin(iconAngle * (M_PI / 180)) * self.pieRadius);
+        
+        SKNode *node = iconsArray[i];
+        node.position = point;
+        
+        [self addChild:node];
+    }
+}
+
+/// Returns a list of ActionIconView items that represent the icons to display on this ActionPieView
+- (NSMutableArray*)getActionIconViewList
+{
+    NSMutableArray *iconsArray = [NSMutableArray array];
+    
+    if(self.actionCollection.actionList.count == 0)
+    {
+        return iconsArray;
+    }
+    
+    if(self.organizeCategories)
+    {
+        BOOL hasAttack = NO;
+        BOOL hasSkill = NO;
+        BOOL hasItems = NO;
+        
+        for (BattleAction *action in self.actionCollection.actionList)
+        {
+            switch(action.actionType)
+            {
+                case ActionTypeAttack:
+                    hasAttack = YES;
+                    break;
+                case ActionTypeItem:
+                    hasItems = YES;
+                    break;
+                case ActionTypeSkill:
+                    hasSkill = YES;
+                    break;
+            }
+        }
+        
+        // Now, create an icon view for each item
+        if(hasSkill)
+        {
+            BattleAction *dummy = [[BattleAction alloc] init]; dummy.actionType = ActionTypeSkill;
+            
+            ActionIconView *icon = [[ActionIconView alloc] initWithAction:dummy];
+            icon.categoryDisplay = YES;
+            
+            [iconsArray addObject:icon];
+        }
+        if(hasAttack)
+        {
+            BattleAction *dummy = [[BattleAction alloc] init]; dummy.actionType = ActionTypeAttack;
+            
+            ActionIconView *icon = [[ActionIconView alloc] initWithAction:dummy];
+            icon.categoryDisplay = YES;
+            
+            [iconsArray addObject:icon];
+        }
+        if(hasItems)
+        {
+            BattleAction *dummy = [[BattleAction alloc] init]; dummy.actionType = ActionTypeItem;
+            
+            ActionIconView *icon = [[ActionIconView alloc] initWithAction:dummy];
+            icon.categoryDisplay = YES;
+            
+            [iconsArray addObject:icon];
+        }
+    }
+    else
+    {
+        for (BattleAction *action in self.actionCollection.actionList)
+        {
+            ActionIconView *icon = [[ActionIconView alloc] initWithAction:action];
+            [iconsArray addObject:icon];
+        }
+    }
+    
+    return iconsArray;
+}
+
+- (void)gameSceneDidReceiveTouchesBegan:(GPGameScene *)gameScene touches:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInNode:gameScene];
+    
+    SKNode *nodeOnPoint = [gameScene nodeAtPoint:point];
+    
+    if(![nodeOnPoint inParentHierarchy:self])
+    {
+        [self close];
+    }
+}
+
+@end
